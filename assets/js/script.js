f4afc16e2f0bb958f25d5d5b179c8eb2d9d45fb1
christanfox <bayshorestinks@aol.com>
2022-02-12 16:54:35 -0500

    data added to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,42 @@
 var fetchTicketsEl = document.querySelector("#ticketMaster");
-var fetchRecipesEl = document.querySelector("#mealDB")
-var requestEventUrl = "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=324&apikey=B7SeHPlCAJjIFj2rmXfNVhHuwqaxGrA7"
+var fetchRecipesEl = document.querySelector("#mealDB");
+var renderRightEl = $("#split-right");
+var requestEventUrl = "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=345&apikey=B7SeHPlCAJjIFj2rmXfNVhHuwqaxGrA7"
 var requestMealUrl = "https://www.themealdb.com/api/json/v1/1/categories.php";
 
-function getTicketsApi() {
+function getTicketsApi(event) {
 event.preventDefault();
 fetch(requestEventUrl)
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
+      var items = data._embedded.events;
+      renderRightEl.empty();
+      for (var i = 0; i < items.length; i++) {
+        var h3 = $("<h3>")
+        h3.text(items[i].name)
+        renderRightEl.append(h3)
+      }
           console.log(data);
     });
 }
 
-function getRecipesApi() {
+function getRecipesApi(event) {
     event.preventDefault();
     fetch(requestMealUrl)
         .then(function(response) {
             return response.json();
           })
           .then(function(mealData) {
-                console.log(mealData);
+            var foods = mealData.categories;
+            renderRightEl.empty();
+            for (var i = 0; i < foods.length; i++) {
+              var h3 = $("<h3>")
+              h3.text(foods[i].strCategory)
+              renderRightEl.append(h3)
+              }  
+             console.log(mealData);
           });
     }
 fetchTicketsEl.addEventListener("click", getTicketsApi);

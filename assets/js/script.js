c75c39b4cdd315f66a2360667459e2476dc4cab4
christanfox <bayshorestinks@aol.com>
2022-02-14 20:02:53 -0500

    preparing for merge

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,22 @@
-var fetchTicketsEl = document.querySelector("#ticketMaster");
-var fetchRecipesEl = document.querySelector("#mealDB");
-var submitBtn = $("#btn");
-var renderRightEl = $(".split-right");
-var requestEventUrl = "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=345&apikey=B7SeHPlCAJjIFj2rmXfNVhHuwqaxGrA7"
-var requestMealUrl = "https://www.themealdb.com/api/json/v1/1/categories.php";
+var fetchTicketsEl = document.querySelector("#searchButton");
+var EventSearchInput = document.querySelector("#eventSearch");
+var dateInput = document.querySelector("#eventDate");
+var locationInput = document.querySelector("#eventLocation")
+var user = JSON.parse(localStorage.getItem("user"));
 
-function getTicketsApi(event) {
+function saveEvents(event) {
 event.preventDefault();
-fetch(requestEventUrl)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      var items = data._embedded.events;
-      renderRightEl.empty();
-      for (var i = 0; i < items.length; i++) {
-        var h3 = $("<h3>")
-        h3.text(items[i].name)
-        renderRightEl.append(h3)
-      }
-          console.log(data);
-    });
+    var user = {
+      eventSearch: EventSearchInput.value,
+      date: dateInput.value,
+      location: locationInput.value
+    };
+
+    localStorage.setItem("user", JSON.stringify(user));
 }
 
-function getRecipesApi(event) {
-    event.preventDefault();
-    fetch(requestMealUrl)
-        .then(function(response) {
-            return response.json();
-          })
-          .then(function(mealData) {
-            var foods = mealData.categories;
-            renderRightEl.empty();
-            for (var i = 0; i < foods.length; i++) {
-              var h3 = $("<h3>")
-              h3.text(foods[i].strCategory)
-              renderRightEl.append(h3)
-              }  
-             console.log(mealData);
-          });
-    }
+    //console.log(user.eventSearch)
+    //console.log(user.date);
+    //console.log(user.location);
 
-
-fetchTicketsEl.addEventListener("click", getTicketsApi);
-fetchRecipesEl.addEventListener("click", getRecipesApi);
+fetchTicketsEl.addEventListener("click", saveEvents);

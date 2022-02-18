e96d1b96b1648a3d06cb8f7c6da09e57586dcde2
njacques47 <nojacques94@gmail.com>
2022-02-18 04:23:03 -0500

    added hardcoded details to make sure API is working and being called properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,28 +13,27 @@ var userInputData = {
 console.log(userInputData)
 
 // get event list using params
-function getEventList (keyword, date, zipcode) {
-  JSON.stringify(userInputData);
+function getEventList () {
+  //JSON.stringify(userInputData);
 
   // stop reload [CURRENTLY NOT WORKING]
-  $("#event-search-btn").click(function(event) {
-    event.preventDefault();
-  });
+  //$("#event-search-btn").click(function(event) {
+    //event.preventDefault();
+  //});
 
-  // display seach results
+  // display search results
   $('#event-panel').show();
 
-
-  // call search events is happening here. confirms its hitting API [DONT KNOW HOW THIS BROKE]
+  // temp hardcoded data to render eventList
   $.ajax({
     type:"GET",
-    url:"https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=B7SeHPlCAJjIFj2rmXfNVhHuwqaxGrA7&" + userInputData.keyword + "&postalCode=" + userInputData.zipcode,
+    url:"https://app.ticketmaster.com/discovery/v2/events?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&keyword=concert&postalCode=10001&locale=*&startDateTime=2022-04-12T03:43:00Z",
     async:true,
     dataType: "json",
     success: function(json) {
-                console.log(json);
-                //getEventList.json = json;
-                
+                getEventList.json = json;
+                displayEventList(json);
+
                 // Parse the response.
                 // Do other things.
              },
@@ -42,14 +41,43 @@ function getEventList (keyword, date, zipcode) {
                 // This time, we do not end up here!
              }
   });
-}
 
 
-//getEventList();
+
+  // call search events is happening here. confirms its hitting API [DONT KNOW HOW THIS BROKE]
+  // $.ajax({
+  //   type:"GET",
+  //   url:"https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=B7SeHPlCAJjIFj2rmXfNVhHuwqaxGrA7&" + userInputData.keyword + "&postalCode=" + userInputData.zipcode,
+  //   async:true,
+  //   dataType: "json",
+  //   success: function(json) {
+  //               console.log(json);
+  //               //getEventList.json = json;
+                
+  //               // Parse the response.
+  //               // Do other things.
+  //            },
+  //   error: function(xhr, status, err) {
+  //               // This time, we do not end up here!
+  //            }
+  // });
+
+
+};
+
+
+getEventList();
 
 // display event list
 function displayEventList () {
+  //hide original text
+  var items = $("#event .list-item")
+  items.hide();
+  //create a variable reference to interact with array
+  //create new elements for display
+  //don't forget error handling
 
+  //display relevant data using modal here
 }
 // get specific event/attraction using id parameter 
 

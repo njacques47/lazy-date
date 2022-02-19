7a232bf6c3e997cc95aa3c4880dbfe328d1c7bc4
christanfox <bayshorestinks@aol.com>
2022-02-19 10:24:35 -0500

    added history list for meal API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,22 @@
+var outButtonEl = document.querySelector("#outButton");
+var startEl = document.querySelector("#start");
+var inButtonEl = document.querySelector("#inButton");
+var eventFormEl = document.querySelector("#eventForm")
 var fetchTicketsEl = document.querySelector("#searchButton");
 var EventSearchInput = document.querySelector("#eventSearch");
 var dateInput = document.querySelector("#eventDate");
-var locationInput = document.querySelector("#eventLocation")
+var randomMeal = $("#mealResults");
+var recipeHistoryEl = $("#recipeHistory")
+var favButtonEl = $("#favButton");
+var locationInput = document.querySelector("#eventLocation");
 var user = JSON.parse(localStorage.getItem("user"));
+var savedMeal = JSON.parse(localStorage.getItem("savedMeal")) || [];
+var requestMealUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
+
+function eventChoice() {
+  startEl.classList.add("hide")
+  eventFormEl.classList.remove("hide")
+}
 
 function saveEvents(event) {
 event.preventDefault();
@@ -15,8 +29,57 @@ event.preventDefault();
     localStorage.setItem("user", JSON.stringify(user));
 }
 
-    console.log(user.eventSearch)
-    console.log(user.date);
-    console.log(user.location);
+function getRecipesApi(event) {
+  event.preventDefault();
+  fetch(requestMealUrl)
+      .then(function(response) {
+          return response.json();
+        })
+        .then(function(mealData) {
+          var meal = mealData.meals;
 
-fetchTicketsEl.addEventListener("click", saveEvents);
+          randomMeal.empty();
+          recipeHistoryEl.empty();
+          for (var i = 0; i < meal.length; i++) {
+            var recipeName = meal[i].strMeal;
+            var recipeImage = meal[i].strMealThumb;
+            var recipeSource = meal[i].strSource;
+            var mealVideo = meal[i].strYoutube;
+
+            var h3 = $("<h3>")
+            h3.text(recipeName)
+            randomMeal.append(h3)
+            var img = $("<img class='shrink' src=" + recipeImage + ">")
+            randomMeal.append(img)
+            var a1 = $("<a href=" + recipeSource + ">Source</a>")
+            randomMeal.append(a1)
+            var a2 = $("<a href=" + mealVideo + ">Video</a>")
+            randomMeal.append(a2)
+
+            var intro = $("<h3>")
+            intro.text("Previous Recipes")
+            recipeHistoryEl.append(intro)
+            for (var i = 0; i < savedMeal.length; i++) {
+              var mealArray = savedMeal[i].mealName;
+              var sourceArray = savedMeal[i].mealSource;         
+              var li = $("<a href='" + sourceArray + "'>" + mealArray + "</a>" + "<br>")
+              recipeHistoryEl.append(li);
+            }
+            var mealAdd = {
+              mealName: recipeName,
+              mealSource: recipeSource
+            }
+            savedMeal.push(mealAdd);
+            if (savedMeal.length > 10) {
+              savedMeal.shift();
+          }  
+            localStorage.setItem("savedMeal", JSON.stringify(savedMeal));
+            }
+
+           console.log(savedMeal);
+        });
+  }
+  
+outButtonEl.addEventListener('click', eventChoice);
+inButtonEl.addEventListener('click', getRecipesApi);
+

809d6794595468980fd4e993bc7586b6861df71b
njacques47 <nojacques94@gmail.com>
2022-02-19 18:59:22 -0500

    my js is so fresh & so clean, clean

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,28 @@
+
+//this isn't working.....yet
+var error = function() {
+  $("#event-form-container").effect("shake");
+};
+
 //when this document is ready, do this
 $(document).ready(function() {
   //make sure to update the form to have this id
   $("#form-submit").submit(function(event) {
-    searchEvents(event);
+    function searchEvents(event) {
+      formData = $("form").serialize();
+    };
+    $("#event-panel").show();
     //stop auto refresh
     event.preventDefault();
     
-  });
-  
+  }); 
 });
 
 // get event list using params
 function searchEvents (event) {
-  formData = $('form').serialize();
-  console.log(formData); //it works [add back up top--its getting passed thru via the event]--this allows us to see what's rendering
-  // temp hardcoded data to render eventList
   $.ajax({
     type:"GET",
-    url:"https://app.ticketmaster.com/discovery/v2/events?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&keyword=concert&postalCode=10001&locale=*&startDateTime=2022-04-12T03:43:00Z",
+    url:"https://app.ticketmaster.com/discovery/v2/events?apikey=B7SeHPlCAJjIFj2rmXfNVhHuwqaxGrA7",
     async:true,
     dataType: "json",
     success: function(json) {
@@ -25,12 +30,11 @@ function searchEvents (event) {
                 displayEventList(json);
              },
     error: function(xhr, status, err) {
-                alert("err"); //temp
+                console.log(err);
              }
   });
 
 };
-
 
 
 // display event list
@@ -41,7 +45,6 @@ function displayEventList (json) {
 
   //create a variable reference to interact with the returned data
   var events = json._embedded.events;
-  console.log(json)
   var item = items.first(); //first matching elements
 
   //loop through data to display
@@ -49,57 +52,21 @@ function displayEventList (json) {
     item.children('.list-item-heading').text(events[i].name);
     item.children('.list-item-info').text(events[i].dates.start.localDate);
 
-    //try looks for it but doesn't throw an error if its not available
+    //try looks for it & will throw an error if its not available
     try {
       item.children('.venue').text(events[i]._embedded.venues[0].name + " in " + events[i]._embedded.venues[0].city.name);
     } catch (err) {
       console.log(err);
     }
     item.show();
-    //item.off("click"); // don't need this???
 
-    
     item.children(".get-tickets").click(events[i], function(eventObject) {
-        // item.children(".get-tickets")
-      //successful return of the request provides event details & calls to 
+      //add event ticket url to btn 
       ticketUrl = eventObject.data.url;
-      console.log(ticketUrl);
-      // $.ajax({
-      //   type:"GET",
-      //   url:"https://app.ticketmaster.com/discovery/v2/events/" + eventObject.data + ".json?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0",
-      //   async:true,
-      //   dataType: "json",
-      //   success: function(json) {
-      //         eventObject.json = json;
-      //         showEventDetails(json);
-      //        },
-      //   error: function(xhr, status, err) {
-      //         console.log("get attraction error");
-      //        }
-      // });
+      $(this).attr("href", ticketUrl);
     });
     item = item.next(); //next matching element replaced here
   }
-};
-
-
-// show event details
-function showEventDetails(json) {
-  console.log("Event details loaded!", json); // it works! now use details to populate eventDetails or suggested events!
-  var items = $("#attractions-container");
-  items.hide();
-
-  //use json to populate name, img
-
-// check img for 
-  $("#attraction-img")
-
-};
-
-//showAttraction();
-
-
-
-// save event 
+}; 
 
 searchEvents();

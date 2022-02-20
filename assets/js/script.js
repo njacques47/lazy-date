7208045cfe56096d12311b05715fabcb0a9820f2
Christan Fox <bayshorestinks@aol.com>
2022-02-20 04:29:32 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,7 @@
-//var outButtonEl = document.querySelector("#outButton");
 var startEl = document.querySelector("#start");
 var inButtonEl = document.querySelector("#inButton");
 var eventFormEl = document.querySelector("#eventForm")
 var fetchTicketsEl = document.querySelector("#searchButton");
-
 var EventSearchInput = document.querySelector("#eventSearch");
 var dateInput = document.querySelector("#eventDate");
 var hideDivEl = document.querySelector("#hide")
@@ -16,25 +14,9 @@ var user = JSON.parse(localStorage.getItem("user"));
 var savedMeal = JSON.parse(localStorage.getItem("savedMeal")) || [];
 var requestMealUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
 
-function eventChoice() {
-  startEl.classList.add("hide")
-  eventFormEl.classList.remove("hide")
-}
-
-function saveEvents(event) {
-event.preventDefault();
-    var user = {
-      eventSearch: EventSearchInput.value,
-      date: dateInput.value,
-      location: locationInput.value
-    };
-
-    localStorage.setItem("user", JSON.stringify(user));
-}
-
 function getRecipesApi(event) {
   event.preventDefault();
-
+// Hides the placeholder information when API renders a recipe
   hideDivEl.classList.add("hide");
   
   fetch(requestMealUrl)
@@ -44,14 +26,18 @@ function getRecipesApi(event) {
         .then(function(mealData) {
           var meal = mealData.meals;
 
+          // Removes the old recipe after generating a new one
           randomMeal.empty();
+          testingEl.empty();
           recipeHistoryEl.empty();
+
           for (var i = 0; i < meal.length; i++) {
             var recipeName = meal[i].strMeal;
             var recipeImage = meal[i].strMealThumb;
             var recipeSource = meal[i].strSource;
             var mealVideo = meal[i].strYoutube;
-
+            
+            // appends the recipe information to the html body
             var h3 = $("<h3 class= 'black'>")
             h3.text(recipeName)
             randomMeal.append(h3)
@@ -62,6 +48,7 @@ function getRecipesApi(event) {
             var a2 = $("<a class='button primary' href=" + mealVideo + ">Video</a>")
             testingEl.append(a2)
 
+            // appends the list of previous results to the html body
             var intro = $("<h3 class= 'black'>")
             intro.text("Previous Recipes")
             recipeHistoryEl.append(intro)
@@ -71,10 +58,13 @@ function getRecipesApi(event) {
               var li = $("<li><a href='" + sourceArray + "'>" + mealArray + "</a>" + "<br></li>")
               recipeHistoryEl.append(li);
             }
+
+            // binds the recipe together with the source html so that they can be called together
             var mealAdd = {
               mealName: recipeName,
               mealSource: recipeSource
             }
+            // limits the amount of saved recipes to 10 and replaces the oldest recipe with the newest one
             savedMeal.push(mealAdd);
             if (savedMeal.length > 10) {
               savedMeal.shift();
@@ -86,3 +76,4 @@ function getRecipesApi(event) {
 
 //outButtonEl.addEventListener('click', eventChoice);
 inButtonEl.addEventListener('click', getRecipesApi);
+
